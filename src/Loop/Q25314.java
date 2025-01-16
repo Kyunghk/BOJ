f1b5b98b4e301fc441baf7176b24661f03c4fec2
kyunghyun <kjsskkh01@kyonggi.ac.kr>
2025-01-16 22:47:56 +0900

    2025-01-16 반복문

---
+++ src/Loop/Q25314.java
@@ -0,0 +1,30 @@
+package Loop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q25314 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int a = N / 4;
+        if (4 <= N && N <= 1000) {
+            if (N % 4 == 0) {
+                for (int i = 0; i < a; i++) {
+                    System.out.print("long ");
+                }
+            } else {
+                System.out.println("조건 위배");
+                return;
+            }
+        } else {
+            System.out.println("조건 위배");
+            return;
+        }
+        System.out.print("int");
+    }
+}
+
+
+
