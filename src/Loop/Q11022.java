91830ad1e9811c338384a6e6fbbfe634aeed4cbb
kyunghyun <kjsskkh01@kyonggi.ac.kr>
2025-01-17 17:11:07 +0900

    2025-01-17 반복문

---
+++ src/Loop/Q11022.java
@@ -0,0 +1,39 @@
+package Loop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Q11022 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        ArrayList<Integer> S = new ArrayList<>();
+        ArrayList<Integer> E = new ArrayList<>();
+        ArrayList<Integer> X = new ArrayList<>();
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if (1 <= A && A <= 9) {
+                if (1 <= B && B <= 9) {
+                    S.add(A);
+                    E.add(B);
+                    X.add(A + B);
+                } else {
+                    return;
+                }
+            } else {
+                return;
+            }
+        }
+
+        for (int i = 0; i < T; i++) {
+            System.out.println("Case #" + (i + 1) + ": " + S.get(i) + " + " + E.get(i) + " = " + X.get(i));
+        }
+
+    }
+}
