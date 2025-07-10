a088084bbd98561a80447ec3f151e198baaadf3b
kyunghyun <kjsskkh01@kyonggi.ac.kr>
2025-07-10 15:54:02 +0900

    Q2558

---
+++ src/Bronze/Q2558.java
@@ -0,0 +1,14 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q2558 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        System.out.println(N+M);
+
+    }
+}
