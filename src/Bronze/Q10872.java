5640fa6b48133cb352765252f073799b3cf912f6
kyunghyun <kjsskkh01@kyonggi.ac.kr>
2025-07-10 15:50:49 +0900

    Q10872

--- src/Bronze/Q10872.java
+++ src/Bronze/Q10872.java
@@ -1,4 +1,19 @@
 package Bronze;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Q10872 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int a = 1;
+        if (N != 0) {
+            for (int i = 1; i <= N; i++) {
+                a = a * i;
+            }
+        }
+        System.out.println(a);
+    }
 }
--- src/Bronze/Q11654.java
+++ src/Bronze/Q11654.java
@@ -1,4 +1,13 @@
 package Bronze;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Q11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = br.readLine().charAt(0);
+        System.out.println(N);
+    }
 }
