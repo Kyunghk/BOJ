d001bd317ee2b427bd5a0cd018df54bbafd9d08b
kyunghyun <kjsskkh01@kyonggi.ac.kr>
2025-07-10 15:57:33 +0900

    Q7287

---
+++ src/Bronze/Q7287.java
@@ -0,0 +1,14 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q7287 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String s1 = br.readLine();
+        System.out.println(s);
+        System.out.println(s1);
+    }
+}
