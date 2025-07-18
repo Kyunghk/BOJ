743ead88e4b3c3aa9c9dcc36d233f054786ee29c
kyunghyun <kjsskkh01@kyonggi.ac.kr>
2025-07-18 15:44:39 +0900

    Q10699

---
+++ src/Bronze/Q10699.java
@@ -0,0 +1,11 @@
+package Bronze;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class Q10699 {
+    public static void main(String[] args) throws IOException {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+    }
+}
