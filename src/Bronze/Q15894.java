e51fdaa673897a9c75fa265af4f4ab1d528c6d42
kyunghyun <kjsskkh01@kyonggi.ac.kr>
2025-11-25 19:26:40 +0900

    feat: Solve Q15894

---
+++ src/Bronze/Q15894.java
@@ -0,0 +1,13 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q15894 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Integer.parseInt(br.readLine());
+        System.out.println(4*N);
+    }
+}
